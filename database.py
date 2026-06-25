bc5c8adcf82d127a4103e53313780ba14948edd3
hinal010 <hinalprajapati010@example.com>
2026-06-25 14:09:13 +0530

    set databaseurl

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Hinal%402554@localhost:5432/attendance_db"
-
+# DATABASE_URL = "postgresql://postgres:Hinal%402554@localhost:5432/attendance_db"
+DATABASE_URL = "postgresql://postgres:YtXTsMQRVkkkohWLcjlxCiMJhmykWvBQ@reseau.proxy.rlwy.net:45259/railway"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
